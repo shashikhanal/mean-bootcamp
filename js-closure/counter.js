7b60780fcc560fd9d2993a2c4b7a14d4c94090b7
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-06-07 11:24:49 +0545

    counter redone

--- js-closure/counter.js
+++ js-closure/counter.js
@@ -1,16 +1,13 @@
-
 // implement counter using private variable in function
 
 var counter = (function(){
 	var count = 0;
-	return {
-			countFunc : function(){
+	return function(){
 			return count += 1;
-		}
 	}
 })();
 
-console.log(counter.countFunc());
-console.log(counter.countFunc());
-console.log(counter.countFunc());
-console.log(counter.countFunc());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
