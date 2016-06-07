960dc13f46e2e3577a3dd8826918a7436e4caf68
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-06-07 11:19:21 +0545

    celebrity function corrected

---
+++ js-closure/celebrity.js
@@ -0,0 +1,27 @@
+/*
+	Create a function CelebrityID that returns a closure that gives you access to a private variable called ID(initially zero).
+
+	The following code should work.
+	var Celeb = CelebrityID();
+	Celeb.get() -> 0
+	Celeb.set(200)
+	Celeb.get() -> 200
+	Celeb.ID -> Access Denied
+*/
+
+var celebrityId = (function(){
+	var id = 0;
+	return {
+		get : function(){
+			return id;
+		},
+		set : function(attr){
+			id = attr;
+		}
+	}
+	return get();
+})();
+
+console.log(celebrityId.get());
+celebrityId.set(200);
+console.log(celebrityId.get());
