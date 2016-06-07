2478697a865b84b0072515bc79b8c837043f5ab4
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-06-07 09:45:17 +0545

    counter done

--- js-closure/counter.js
+++ js-closure/counter.js
@@ -1,14 +1,5 @@
 
-// Create a function called Counter(). When called it should return a function that increases a private variable called count(initialized to zero) and return the new value of counter.
-
-// Basically, this code should work
-// var counter = counter()
-// counter() -> 1
-// counter() -> 2
-// counter() -> 3
-// counter() -> 4
-
-// There should be no way to access the count variable directly.
+// implement counter using private variable in function
 
 var counter = (function(){
 	var count = 0;
