fa27cf9f3b5b11f8d81e30e6d13f8f4cdb952171
Shashi Khanal <dragonSlayer@dragonSlayer.local>
2016-06-21 14:44:01 +0545

    js file for test cases

---
+++ js/className.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+	addClass : function (el, newClass){
+		if(el.className.indexOf(newClass) !== -1){
+			return;
+		}
+
+		if(el.className !==''){
+			newClass = ' ' + newClass;
+		}
+
+		el.className += newClass;
+	}
+}
